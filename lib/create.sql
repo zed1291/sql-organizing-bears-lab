1d850825607917306e761c323535d5d7e34c5d9a
“zed” <zed@kolk.me>
2021-01-24 18:41:38 -0600

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,13 @@
+CREATE TABLE bears (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+sex TEXT,
+color TEXT,
+temperament TEXT,
+alive INTEGER
+);
+
+-- SELECT column_name, column_name
+-- FROM table_name
+-- ORDER BY column_name ASC, column_name DESC; 
---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES ()
+
+
+-- INSERT INTO cats (id, name, age, breed, net_worth) VALUES (1, "Maru", 3, "Scottish Fold", 1000000);
+-- INSERT INTO cats (id, name, age, breed, net_worth) VALUES (2, "Hana", 1, "Tabby", 21800);
+-- INSERT INTO cats (id, name, age, breed, net_worth) VALUES (3, "Grumpy Cat", 4, "Persian", 181600);
+-- INSERT INTO cats (id, name, age, breed, net_worth) VALUES (4, "Lil\' Bub", 2, "Tortoiseshell", 2000000);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
